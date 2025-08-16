fc6728f0e20e1dd5ea7db5f254d525cd0c58e3eb
Your Name <kadengoldasteam@gmail.com>
2025-08-16 15:39:08 -0700

    Regen RUN 1:
    Mode:Tesla
    regen_torqueLimitDNm = 500
    regen_torqueAtZeroPedalDNm = me->regen_torqueLimitDNm;
    regen_percentAPPSForCoasting = .1;
    regen_percentBPSForMaxRegen = 0;

--- dev/main.c
+++ dev/main.c
@@ -138,6 +138,8 @@ void main(void)
     ubyte4 timestamp_startTime = 0;
     ubyte4 timestamp_EcoButton = 0;
     ubyte1 calibrationErrors; //NOT USED
+    ubyte1 tick =0;
+
 
     /*******************************************/
     /*        Low Level Initializations        */
@@ -323,13 +325,21 @@ void main(void)
         //if (IO_RTC_GetTimeUS(timestamp_calibStart) < (ubyte4)5000000)
 
         //SensorValue TRUE and FALSE are reversed due to Pull Up Resistor
-
         //No regen below 5kph
+        if (MCM_getDCCurrent < -72)
+        {
+            tick++;
+        }
+        else {
+            tick = 0;
+        }
+        
         sbyte4 groundSpeedKPH = MCM_getGroundSpeedKPH(mcm0);
-        if (groundSpeedKPH < 15)
+        if (groundSpeedKPH < 15 || tick>=10)
         {
             MCM_setRegenMode(mcm0, REGENMODE_OFF);
         } else {
+            MCM_setRegenMode(mcm0, REGENMODE_TESLA);
             // Regen mode is now set based on battery voltage to preserve overvoltage fault 
             // if(BMS_getPackVoltage(bms) >= 38500 * 10){ 
             //     MCM_setRegenMode(mcm0, REGENMODE_FORMULAE); 
@@ -422,7 +432,8 @@ void main(void)
         //Assign motor controls to MCM command message
         //motorController_setCommands(rtds);
         //DOES NOT set inverter command or rtds flag
-        MCM_setRegenMode(mcm0, REGENMODE_FORMULAE); // TODO: Read regen mode from DCU CAN message - Issue #96
+        //if (mcm0->regen_mode != REGENMODE_OFF)
+       // MCM_setRegenMode(mcm0, REGENMODE_FORMULAE); // TODO: Read regen mode from DCU CAN message - Issue #96
         // MCM_readTCSSettings(mcm0, &Sensor_TCSSwitchUp, &Sensor_TCSSwitchDown, &Sensor_TCSKnob);
         PID_setSaturationPoint(lcPID, 231);
         LaunchControl_calculateTorqueCommand(lc, tps, bps, mcm0,lcPID);
