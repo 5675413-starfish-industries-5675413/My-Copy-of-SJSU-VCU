6fff5b61a2c40ca4af9fe18fd7548c0f5cc7d182
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-10-03 18:10:40 -0700

    Remove redundant torque output checks

--- dev/motorController.c
+++ dev/motorController.c
@@ -331,16 +331,6 @@ void MCM_calculateCommands(MotorController *me, TorqueEncoder *tps, BrakePressur
     {
         torqueOutput = me->appsTorque;
     }
-
-    if(torqueOutput > 2310)
-    {
-        torqueOutput=2310;
-    }
-    
-    if (torqueOutput<-2310 )
-    {
-        torqueOutput=-2310;
-    }
     MCM_commands_setTorqueDNm(me, torqueOutput);
 
     //Causes MCM relay to be driven after 30 seconds with TTC60?
