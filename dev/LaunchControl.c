c84be296c3a4fc7656088a9d2f1123bf8e3eb98c
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-10-04 17:57:04 -0700

    run 10

--- dev/LaunchControl.c
+++ dev/LaunchControl.c
@@ -27,9 +27,9 @@ LaunchControl *LaunchControl_new(bool lcToggle)
     me->lcToggle = lcToggle;
     me->slipRatio = 0;
     me->lcTorqueCommand = 0;
-    me->k = 0.05;
+    me->k = 0.6;
     me->maxTorque = 231;
-    me->prevTorque = 100;
+    me->prevTorque = 0;
     me->isInitialCurve = FALSE;
     me->mode = SLIP_CONTROLLER;
     me->state = LC_IDLE;
