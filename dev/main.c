30982778b4e0cebf5667e171e7bb50e3500a3b2c
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-10-05 15:03:19 -0700

    PL run 6 v2

--- dev/main.c
+++ dev/main.c
@@ -214,7 +214,7 @@ void main(void)
     ReadyToDriveSound *rtds = RTDS_new();
     BatteryManagementSystem *bms = BMS_new(serialMan, BMS_BASE_ADDRESS);
     // 240 Nm
-    MotorController *mcm0 = MotorController_new(serialMan, 0xA0, FORWARD, 1950, 5, 10); //CAN addr, direction, torque limit x10 (100 = 10Nm)
+    MotorController *mcm0 = MotorController_new(serialMan, 0xA0, FORWARD, 2400, 5, 10); //CAN addr, direction, torque limit x10 (100 = 10Nm)
     // 75 Nm
     InstrumentCluster *ic0 = InstrumentCluster_new(serialMan, 0x702);
     TorqueEncoder *tps = TorqueEncoder_new(bench);
