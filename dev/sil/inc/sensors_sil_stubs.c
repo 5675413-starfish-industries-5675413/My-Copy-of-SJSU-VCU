4aee199f0ca5e1e732855860924607484afa94f9
flyingpenguinie <seongkang05@gmail.com>
2025-10-22 23:58:09 -0700

    Moved sensor stubs to inc folder

---
+++ dev/sil/inc/sensors_sil_stubs.c
@@ -0,0 +1,26 @@
+#include "sensors.h"
+
+// Define all global sensor objects for SIL testing
+// These are referenced as extern in various files but need to be defined somewhere
+
+Sensor Sensor_TPS0;
+Sensor Sensor_TPS1;
+Sensor Sensor_BPS0;
+Sensor Sensor_BPS1;
+Sensor Sensor_WSS_FL;
+Sensor Sensor_WSS_FR;
+Sensor Sensor_WSS_RL;
+Sensor Sensor_WSS_RR;
+Sensor Sensor_RTDButton;
+Sensor Sensor_EcoButton;
+Sensor Sensor_LCButton;
+Sensor Sensor_HVILTerminationSense;
+Sensor Sensor_DRSButton;
+Sensor Sensor_TestButton;
+Sensor Sensor_LVBattery;
+Sensor Sensor_SAS;
+Sensor Sensor_DRSKnob;
+Sensor Sensor_PLKnob;
+Sensor Sensor_BenchTPS0;
+Sensor Sensor_BenchTPS1;
+
