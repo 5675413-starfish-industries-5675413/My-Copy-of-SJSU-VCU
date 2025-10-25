83b9d1cf3aafe0fd4e402b0e847818894082d4cd
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-10-25 10:45:57 -0700

    fix return value for getters

--- dev/brakePressureSensor.h
+++ dev/brakePressureSensor.h
@@ -51,8 +51,8 @@ void BrakePressureSensor_startCalibration(BrakePressureSensor *me, ubyte1 second
 void BrakePressureSensor_calibrationCycle(BrakePressureSensor *me, ubyte1 *errorCount);
 void BrakePressureSensor_getPedalTravel(BrakePressureSensor *me, ubyte1 *errorCount, float4 *pedalPercent);
 void BrakePressureSensor_setPSI(BrakePressureSensor *me);
-float4 BrakePressureSensor_getBPS0_mV(BrakePressureSensor *me);
-float4 BrakePressureSensor_getBPS1_mV(BrakePressureSensor *me);
+ubyte2 BrakePressureSensor_getBPS0_mV(BrakePressureSensor *me);
+ubyte2 BrakePressureSensor_getBPS1_mV(BrakePressureSensor *me);
 ubyte2 BrakePressureSensor_getBPS0_Pressure(BrakePressureSensor *me);
 ubyte2 BrakePressureSensor_getBPS1_Pressure(BrakePressureSensor *me);
 
