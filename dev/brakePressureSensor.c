8ca7a592f5b16aa20b8fa0c2254ba9c90417423a
Vern-00 <verntoor23@gmail.com>
2025-10-25 16:57:34 -0700

    fixes

--- dev/brakePressureSensor.c
+++ dev/brakePressureSensor.c
@@ -80,8 +80,6 @@ BrakePressureSensor *BrakePressureSensor_new(void)
 
 void BrakePressureSensor_setPSI(BrakePressureSensor *me){
     // backcalc: ADC counts to volts (10-bit, 5 V)
-    me->bps0_V = me->bps0_value / ADC_RESOLUTION_COUNT * ADC_VOLT_RANGE;     // front: 0.487-4.487
-    me->bps1_V = me->bps1_value / ADC_RESOLUTION_COUNT * ADC_VOLT_RANGE;     // rear:  0.495-4.495
     me->bps0_V = me->bps0_value / ADC_RESOLUTION_COUNT * ADC_VOLT_RANGE - 2.5;     // front: 0.487-4.487
     me->bps1_V = me->bps1_value / ADC_RESOLUTION_COUNT * ADC_VOLT_RANGE - 2.5;     // rear:  0.495-4.495
 
