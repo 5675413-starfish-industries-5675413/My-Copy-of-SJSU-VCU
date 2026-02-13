35e7b88e948de5ca0a81ad8e252c90f78880f257
Seong Kang <seongkang05@gmail.com>
2026-02-12 19:02:12 -0800

    Added helper methods for abstraction

--- dev/test/sre_test/sil/core/helpers/path.py
+++ dev/test/sre_test/sil/core/helpers/path.py
@@ -6,12 +6,13 @@ Provides common paths as global constants.
 from pathlib import Path
 
 # Start from this file's location
-# This file is at: sre_test/sil/core/paths.py
-_core_dir = Path(__file__).parent  # sre_test/sil/core/
-_sil_dir = _core_dir.parent  # sre_test/sil/
-_sre_test_dir = _sil_dir.parent  # sre_test/
-_test_dir = _sre_test_dir.parent  # test/
-_dev_dir = _test_dir.parent / 'dev'  # dev/
+# This file is at: dev/test/sre_test/sil/core/helpers/path.py
+_helpers_dir = Path(__file__).parent  # dev/test/sre_test/sil/core/helpers/
+_core_dir = _helpers_dir.parent  # dev/test/sre_test/sil/core/
+_sil_dir = _core_dir.parent  # dev/test/sre_test/sil/
+_sre_test_dir = _sil_dir.parent  # dev/test/sre_test/
+_test_dir = _sre_test_dir.parent  # dev/test/
+_dev_dir = _test_dir.parent  # dev/
 _root_dir = _dev_dir.parent  # VCU root/
 
 # Global path constants
@@ -26,6 +27,7 @@ CORE = _core_dir
 # SIL-specific directories
 CONFIG = _sil_dir / 'config'
 TESTS = _sil_dir / 'core' / 'tests'
+CLI_TESTS = _sil_dir / 'cli' / 'tests'
 DATA = _sil_dir / 'tests' / 'data'
 
 # Common files
