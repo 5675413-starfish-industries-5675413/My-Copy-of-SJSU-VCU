ff2581f5d31030472c643bd16272aa6a7e6b2c47
Crispus Hong <86194089+crispush@users.noreply.github.com>
2026-03-24 22:32:55 -0700

    added big endian parsing function for ubyte2 and parsing functions renamed to specify little or big endian

--- dev/shunt.c
+++ dev/shunt.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "shunt.h"
 
-static ubyte4 Shunt_parseUbyte4(ubyte1* data);
-static sbyte4 Shunt_parseSbyte4(ubyte1* data);
-static unsigned long long Shunt_parseUint64(ubyte1* data);
-static long long Shunt_parseInt64(ubyte1* data);
+static ubyte4 Shunt_parseUbyte4LE(ubyte1* data);
+static sbyte4 Shunt_parseSbyte4LE(ubyte1* data);
+static unsigned long long Shunt_parseUint64LE(ubyte1* data);
+static long long Shunt_parseInt64LE(ubyte1* data);
+
+static ubyte2 Shunt_parseUbyte2BE(ubyte1* data);
 
 Shunt *Shunt_new(void){
     Shunt *me = (Shunt *)malloc(sizeof(Shunt));
@@ -18,32 +20,32 @@ void Shunt_parseCanMessage(Shunt *shunt, IO_CAN_DATA_FRAME *shuntCanMessage)
 {
     switch(shuntCanMessage->id){
         case SHUNT_CURRENT_ID:
-            shunt->current_mA = Shunt_parseSbyte4(shuntCanMessage->data);
+            shunt->current_mA = Shunt_parseSbyte4LE(shuntCanMessage->data);
             break;
         case SHUNT_TEMPERATURE_ID:
-            shunt->temperature_dC = Shunt_parseSbyte4(shuntCanMessage->data);
+            shunt->temperature_dC = Shunt_parseSbyte4LE(shuntCanMessage->data);
             break;
         case SHUNT_VBUS_ID:
-            shunt->vBus_mV = Shunt_parseSbyte4(shuntCanMessage->data);
+            shunt->vBus_mV = Shunt_parseSbyte4LE(shuntCanMessage->data);
             break;
         case SHUNT_COULOMB_ID:
-            shunt->coulombCount = Shunt_parseInt64(shuntCanMessage->data);
+            shunt->coulombCount = Shunt_parseInt64LE(shuntCanMessage->data);
             break;
         case SHUNT_POWER_ID:
-            shunt->power_dW = Shunt_parseUbyte4(shuntCanMessage->data);
+            shunt->power_dW = Shunt_parseUbyte4LE(shuntCanMessage->data);
             break;
         case SHUNT_ENERGY_ID:
-            shunt->energy_Wh = Shunt_parseUint64(shuntCanMessage->data);
+            shunt->energy_Wh = Shunt_parseUint64LE(shuntCanMessage->data);
             break;
         case SHUNT_ERRORS_ID:
-            shunt->errors = (ubyte2) (shuntCanMessage->data[1] ) | (ubyte2) (shuntCanMessage->data[0] << 8);
+            shunt->errors = Shunt_parseUbyte2BE(shuntCanMessage->data);
             break;
         default:
             break;
         }
 }
 
-static ubyte4 Shunt_parseUbyte4(ubyte1* data){
+static ubyte4 Shunt_parseUbyte4LE(ubyte1* data){
     return (ubyte4)(
         ((ubyte4) data[0])          |
         ((ubyte4) data[1] << 8)     |
@@ -52,23 +54,31 @@ static ubyte4 Shunt_parseUbyte4(ubyte1* data){
     );
 }
 
-static sbyte4 Shunt_parseSbyte4(ubyte1* data){
-    return (sbyte4) Shunt_parseUbyte4(data);
+static sbyte4 Shunt_parseSbyte4LE(ubyte1* data){
+    return (sbyte4) Shunt_parseUbyte4LE(data);
 }
 
-unsigned long long Shunt_parseUint64(ubyte1 *data){
+unsigned long long Shunt_parseUint64LE(ubyte1 *data){
     return (unsigned long long)(
-        ((unsigned long long)data[0])        |
-        ((unsigned long long)data[1] << 8)   |
-        ((unsigned long long)data[2] << 16)  |
-        ((unsigned long long)data[3] << 24)  |
-        ((unsigned long long)data[4] << 32)  |
-        ((unsigned long long)data[5] << 40)  |
-        ((unsigned long long)data[6] << 48)  |
-        ((unsigned long long)data[7] << 56)
+        ((unsigned long long) data[0])        |
+        ((unsigned long long) data[1] << 8)   |
+        ((unsigned long long) data[2] << 16)  |
+        ((unsigned long long) data[3] << 24)  |
+        ((unsigned long long) data[4] << 32)  |
+        ((unsigned long long) data[5] << 40)  |
+        ((unsigned long long) data[6] << 48)  |
+        ((unsigned long long) data[7] << 56)
     );
 }
 
-long long Shunt_parseInt64(ubyte1 *data){
-    return (long long) Shunt_parseUint64(data);
+long long Shunt_parseInt64LE(ubyte1 *data){
+    return (long long) Shunt_parseUint64LE(data);
+}
+
+ubyte2 Shunt_parseUbyte2BE(ubyte1 *data)
+{
+    return (ubyte2)(
+        ((ubyte2) data[1])  |
+        ((ubyte2) data[0] << 8)
+    );
 }
