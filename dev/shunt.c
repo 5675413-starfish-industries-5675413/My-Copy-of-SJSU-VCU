9edb08069d371e73abeefda30c271de68b0ff032
Crispus Hong <86194089+crispush@users.noreply.github.com>
2026-03-24 22:24:54 -0700

    added shunt sensor readings fields (temperature, vbus, power, energy, errors) with updated CAN message parser

--- dev/shunt.c
+++ dev/shunt.c
@@ -3,6 +3,7 @@
 
 static ubyte4 Shunt_parseUbyte4(ubyte1* data);
 static sbyte4 Shunt_parseSbyte4(ubyte1* data);
+static unsigned long long Shunt_parseUint64(ubyte1* data);
 static long long Shunt_parseInt64(ubyte1* data);
 
 Shunt *Shunt_new(void){
@@ -19,8 +20,23 @@ void Shunt_parseCanMessage(Shunt *shunt, IO_CAN_DATA_FRAME *shuntCanMessage)
         case SHUNT_CURRENT_ID:
             shunt->current_mA = Shunt_parseSbyte4(shuntCanMessage->data);
             break;
+        case SHUNT_TEMPERATURE_ID:
+            shunt->temperature_dC = Shunt_parseSbyte4(shuntCanMessage->data);
+            break;
+        case SHUNT_VBUS_ID:
+            shunt->vBus_mV = Shunt_parseSbyte4(shuntCanMessage->data);
+            break;
         case SHUNT_COULOMB_ID:
             shunt->coulombCount = Shunt_parseInt64(shuntCanMessage->data);
+            break;
+        case SHUNT_POWER_ID:
+            shunt->power_dW = Shunt_parseUbyte4(shuntCanMessage->data);
+            break;
+        case SHUNT_ENERGY_ID:
+            shunt->energy_Wh = Shunt_parseUint64(shuntCanMessage->data);
+            break;
+        case SHUNT_ERRORS_ID:
+            shunt->errors = (ubyte2) (shuntCanMessage->data[1] ) | (ubyte2) (shuntCanMessage->data[0] << 8);
             break;
         default:
             break;
@@ -40,16 +56,19 @@ static sbyte4 Shunt_parseSbyte4(ubyte1* data){
     return (sbyte4) Shunt_parseUbyte4(data);
 }
 
-long long Shunt_parseInt64(ubyte1 *data)
-{
-    return (long long)(
-        ((long long)data[0])        |
-        ((long long)data[1] << 8)   |
-        ((long long)data[2] << 16)  |
-        ((long long)data[3] << 24)  |
-        ((long long)data[4] << 32)  |
-        ((long long)data[5] << 40)  |
-        ((long long)data[6] << 48)  |
-        ((long long)data[7] << 56)
+unsigned long long Shunt_parseUint64(ubyte1 *data){
+    return (unsigned long long)(
+        ((unsigned long long)data[0])        |
+        ((unsigned long long)data[1] << 8)   |
+        ((unsigned long long)data[2] << 16)  |
+        ((unsigned long long)data[3] << 24)  |
+        ((unsigned long long)data[4] << 32)  |
+        ((unsigned long long)data[5] << 40)  |
+        ((unsigned long long)data[6] << 48)  |
+        ((unsigned long long)data[7] << 56)
     );
+}
+
+long long Shunt_parseInt64(ubyte1 *data){
+    return (long long) Shunt_parseUint64(data);
 }
--- dev/shunt.h
+++ dev/shunt.h
@@ -5,15 +5,22 @@
 #include "IO_CAN.h"
 
 #define SHUNT_CURRENT_ID            0x3F1
+#define SHUNT_TEMPERATURE_ID        0x3F2
+#define SHUNT_VBUS_ID               0x3F3
 #define SHUNT_COULOMB_ID            0x3F4
-#define SHUNT_SET_COMMAND_ID        0x3FA
-#define SHUNT_GET_COMMAND_ID        0x3FB
-#define SHUNT_REPLY_ID              0x3FC
+#define SHUNT_POWER_ID              0x3F5
+#define SHUNT_ENERGY_ID             0x3F6
+#define SHUNT_ERRORS_ID             0x3F7
 
 typedef enum{
     SHUNT_COMMAND_GET_ALL_ENABLED    = 0x00,
     SHUNT_COMMAND_GET_CURRENT       = 0x01,
+    SHUNT_COMMAND_GET_TEMPERATURE   = 0x02,
+    SHUNT_COMMAND_GET_VBUS          = 0x03,
     SHUNT_COMMAND_GET_COULOMB       = 0x04,
+    SHUNT_COMMAND_GET_POWER         = 0x05,
+    SHUNT_COMMAND_GET_ENERGY        = 0x06,
+    SHUNT_COMMAND_GET_ERRORS        = 0x07,
     SHUNT_COMMAND_RESET             = 0x10,
     SHUNT_COMMAND_SETMODE           = 0x12,
     SHUNT_COMMAND_READING_DELAY     = 0x16
@@ -22,7 +29,12 @@ typedef enum{
 
 typedef struct _Shunt{
     sbyte4 current_mA;
+    sbyte4 temperature_dC;
+    sbyte4 vBus_mV;
     long long coulombCount;
+    ubyte4 power_dW;
+    unsigned long long energy_Wh;
+    ubyte2 errors;
 } Shunt;
 
 Shunt *Shunt_new(void);
